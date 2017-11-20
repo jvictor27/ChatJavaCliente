216235c9097ff4124eaff8f0d22b2e8422dd35cf
jvictor27 <jvictor451@gmail.com>
2017-11-20 08:06:15 -0200

    Cliente do chat java, incompleto

---
+++ src/br/edu/ifpb/pod/bean/ChatMessage.java
@@ -0,0 +1,74 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpb.pod.bean;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author João Victor
+ * 
+ * 
+ */
+public class ChatMessage implements Serializable {
+    
+    private String name;
+    private String text;
+    
+    // Armazena nome do cliente que receberá mensagem reservada
+    private String nameReserved;
+    
+    // Armazena todos os cientes online/conectados no servidor, enquanto servidor ativo
+    private Set<String> setOnlines = new HashSet<String>();
+    
+    private Action action;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getNameReserved() {
+        return nameReserved;
+    }
+
+    public void setNameReserved(String nameReserved) {
+        this.nameReserved = nameReserved;
+    }
+
+    public Set<String> getSetOnlines() {
+        return setOnlines;
+    }
+
+    public void setSetOnlines(Set<String> setOnlines) {
+        this.setOnlines = setOnlines;
+    }
+
+    public Action getAction() {
+        return action;
+    }
+
+    public void setAction(Action action) {
+        this.action = action;
+    }
+        
+    // Ações a serem executados pelo cliente no chat
+    public enum Action {
+        CONNECT, DISCONNECT, SEND_ONE, SEND_ALL, USERS_ONLINE
+    }
+}
---
+++ src/br/edu/ifpb/pod/service/ClienteService.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpb.pod.service;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import br.edu.ifpb.pod.bean.ChatMessage;
+
+/**
+ *
+ * @author João Victor
+ */
+public class ClienteService {
+    
+    private Socket socket;
+    private ObjectOutputStream output;
+    
+    public Socket connect() {
+        try {
+            this.socket = new Socket("localhost", 5555);
+            this.output = new ObjectOutputStream(socket.getOutputStream());
+        } catch (UnknownHostException ex) {
+            Logger.getLogger(ClienteService.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return socket;
+    }
+    
+    public void send(ChatMessage message) {
+        try {
+            output.writeObject(message);
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
